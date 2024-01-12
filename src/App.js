9dcb0854f8df4722d5334439e9b7430f27f26429
jaysohagiya54 <jaysuhagiya80@gmail.com>
2024-01-12 10:25:03 +0530

    chore: first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header"
+import Sidebar from './components/Sidebar';
+import Body from './components/Body';
+import { Provider } from 'react-redux';
+import store from './store';
+function App() {
+  return (
+    <Provider store={store}>
+    <div className='bg-[#d6f0fc]'>
+      <Header />
+    <div className="App flex pt-3">
+       <Sidebar/>
+       <Body/>
+    </div>
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
